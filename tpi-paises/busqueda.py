75e271a7ccf5da4200da7c94300b29654c34a89c
Mateo Algañaraz <alganarazmateo@gmail.com>
2025-10-30 08:36:29 -0300

    Mejor legibilidad

--- tpi-paises/busqueda.py
+++ tpi-paises/busqueda.py
@@ -9,21 +9,18 @@ def _input_no_vacio(mensaje):
 
 def buscar_pais(paises):
     """Función para buscar países por su nombre dentro de la lista 'paises'"""
-
     nombre = _input_no_vacio("Ingrese nombre (o parte del nombre) del país: ")
     resultados = [p for p in paises if nombre.lower() in p['nombre'].lower()]
     mostrar_paises(resultados)
 
 def filtrar_continente(paises):
     """Muestra todos los países del continente ingresado que haya dentro de la lista 'paises """
-
     continente = _input_no_vacio("Ingrese continente (América, Europa, Asia, África, Oceanía): ")
     resultados = [p for p in paises if p['continente'].lower() == continente.lower()]
     mostrar_paises(resultados)
 
 def filtrar_poblacion(paises):
     """Muestra todos los países que hay dentro de un rango mínimo y máximo de población dentro de la lista 'paises'"""
-
     while True:
         try:
             min_pob = int(input("Población mínima: "))
@@ -38,7 +35,6 @@ def filtrar_poblacion(paises):
 
 def filtrar_superficie(paises):
     """Muestra todos los países que hay dentro de un rango mínimo y máximo de superficie dentro de la lista 'paises'"""
-
     while True:
         try:
             min_sup = int(input("Superficie mínima (km²): "))
@@ -53,7 +49,6 @@ def filtrar_superficie(paises):
 
 def ordenar_paises_opcion(paises):
     """Ordena todos los países de la lista 'paises' a través del criterio elegido por el usuario"""
-    
     while True:
         print("Ordenar por:\na) Nombre\nb) Población\nc) Superficie")
         criterio = input("Elija (a/b/c): ").strip().lower()
